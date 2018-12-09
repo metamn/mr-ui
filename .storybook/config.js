05c0326724d59ca728b13715764a02e9ead270ef
metamn <bartus.csongor@gmail.com>
2018-12-09 17:52:19 +0200

    A11y Storybook addon added

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,14 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { checkA11y } from '@storybook/addon-a11y';
 import '@storybook/addon-console';
 
+// Pick all stories.js files within the src/ folder
 const req = require.context('../src', true, /.stories.js$/);
 
+// Globally avaialable addons
+addDecorator(checkA11y);
+
+// Automatic story loader
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
