2a8dfcff5d3dea28f74ecd57d8de9f036ae4499a
metamn <bartus.csongor@gmail.com>
2018-12-09 16:33:59 +0200

    Storybook added

---
+++ src/App.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import App from './App';
+
+storiesOf('App', module)
+	.add('default', () => <App />)
