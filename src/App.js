ed056e919ab53c8415f02cdf229741bb7e3b4875
metamn <bartus.csongor@gmail.com>
2018-12-09 16:25:56 +0200

    Styled components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
 
 class App extends Component {
-  render() {
-    return (
-      'app'
-    );
-  }
+	render() {
+		return (
+			<Container>
+			'app'
+			</Container>
+		);
+	}
 }
 
 export default App;
